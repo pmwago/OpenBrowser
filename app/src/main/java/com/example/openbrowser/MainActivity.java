0fa719280837ea803780b83a314509b07ae9cf7b
p.mwago <ptrmwago@gmail.com>
2021-11-30 22:17:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/openbrowser/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.openbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBrowser();
+            }
+        });
+    }
+    public void openBrowser(){
+        String url="http://www.google.com";
+        Intent open=new Intent(Intent.ACTION_VIEW);
+        open.setData(Uri.parse(url));
+        startActivity(open);
+    }
+}
